5c4d1bed677f3459854ac22f52a43a94237da410
jrnature <jrnature@gmail.com>
2024-09-18 23:52:11 -0600

    Actualización de creación y fondeo de cuenta

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
+import { useState } from "react";
+
 function App() {
+  const [valor, setValor] = useState(0)
+
+  const incrementarContador = () => {
+    setValor(valor + 1)
+  }
+
+  const decrementarContador = () => {
+    setValor(valor - 1)
+  }
+
+  const inicializarContador = () =>{
+    setValor(0)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>El valor del contador = {valor}</p>
+      <button onClick={incrementarContador}>Aumentar Contador</button>
+      <button onClick={decrementarContador}>Disminuir Contador</button>
+      <button onClick={inicializarContador}>Resetear Contador</button>
     </div>
   );
 }
---
+++ src/App2.js
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import { Aptos, AptosConfig, Network, Account} from "@aptos-labs/ts-sdk";
+
+const config= new AptosConfig ({network : Network.TESTNET});
+const aptos= new Aptos(config);
+
+const APTOS_COIN="0X1::aptos_coin::AptosCoin";
+const COIN_STORE=`0x01::coin::CoinStore<${APTOS_COIN}>`;
+
+function App2() {
+  const [valor, setValor] = useState(0)
+
+  const crearCuenta =async ()=>{
+    const alicia = Account.generate();
+    console.log(alicia.accountAddress);
+    setValor(alicia.accountAddress);
+  }
+
+  const fondearCuenta= async ()=>{
+    await aptos.fundAccount({
+        accountAddress: valor,
+        amount : 100_000_000,
+    });
+
+    console.log(`La cuenta ${valor} fue dondeada`)
+  }
+
+  const saldoCuenta = async () => {
+    const saldoAlicia = await aptos.getAccountResource ({
+        accountAddress: valor,
+        resourceType: COIN_STORE,
+    });
+   console.log(`El saldo de la cuenta es: ${saldoAlicia.coin.value}`)
+  }
+
+  return (
+    <div className="App2">
+        <button onClick={crearCuenta}>Generar Cuenta</button>
+        <button onClick={fondearCuenta}>Fondear Cuenta</button>
+        <button onClick={saldoCuenta}>Saldo de la Cuenta</button>
+    </div>
+  );
+}
+
+export default App2;
